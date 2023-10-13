42318416133b5caa9599d7dd2e10b910970f8d4a
rmartinezapp <robert.martinez.app@gmail.com>
2023-10-13 18:27:57 -0300

    fix link menu

--- app/overrides/add_logistic_operator_to_admin_configuration.rb
+++ app/overrides/add_logistic_operator_to_admin_configuration.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(
   virtual_path: 'spree/admin/shared/sub_menu/_configuration',
-  name: 'add_social_providers_link_configuration_menu',
-  insert_before: '[data-hook="admin_configurations_sidebar_menu"]',
+  name: 'add_logistic_operator_link_configuration_menu',
+  insert_bottom: '[data-hook="admin_configurations_sidebar_menu"]',
   text: '<%= configurations_sidebar_menu_item plural_resource_name(Spree::LogisticOperator), spree.admin_logistic_operators_path %>'
 )
